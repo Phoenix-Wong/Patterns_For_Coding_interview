4224c05e332b2181faa3f5e10911c05db348f738
Akshant Vats <AkshantVats@outlook.com>
2020-07-18 16:03:22 +0530

    Added First Solution

---
+++ Java/Sliding_Window_Pattern_Practice_Questions/AverageOfSubArrayOfSizeK.java
@@ -0,0 +1,40 @@
+/*
+Question : Given an array, find the average of all contiguous sub-arrays of size ‘K’ in it.
+*/
+
+import java.util.Arrays; 
+ 
+class AverageOfSubArrayOfSizeK
+{
+    public static void main (String args[])
+    {
+
+        int k = 5;
+        int[] arr = new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2};
+
+        double[] result = AverageOfSubArrayOfSizeK.findAverages(arr, k);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+
+
+    public static double[] findAverages(int[] arr, int k)
+    {
+        double[] result = new double[arr.length - k + 1];
+        int windowStart = 0;
+        double windowSum = 0;
+
+        for(int windowEnd = 0; windowEnd < arr.length ; windowEnd++)
+        {
+            windowSum += arr[windowEnd];
+
+            if (windowEnd >= k - 1) {
+                result[windowStart] = windowSum / k;
+                windowSum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+        return result;
+    }
+}
--- README.md
+++ README.md
@@ -9,7 +9,6 @@
 
 ## Sliding_Window_Pattern_Practice_Questions
 
-| # | Title | Solution | Basic idea (One line) |
-|---| ----- | -------- | --------------------- |
-| 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Java](https://github.com/qiyuangong/leetcode/blob/master/java/001_Two_Sum.java) | 1. Hash O(n) and O(n) space.<br>2. Sort and search with two points O(n) and O(1) space. |
-| 2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [Java](https://github.com/qiyuangong/leetcode/blob/master/java/002_Add_Two_Numbers.java) | Take care of the carry from lower digit. |  
+| # | Question | Solution |
+|---| -------- | -------- |
+| 1 | Given an array, find the average of all contiguous sub-arrays of size ‘K’ in it. | [Java](https://github.com/qiyuangong/leetcode/blob/master/java/001_Two_Sum.java) |
