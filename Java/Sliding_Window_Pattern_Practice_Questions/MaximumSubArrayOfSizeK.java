5a03a8e0e056850608be39b0cc0d943fa656e4fe
pengfei.wang <Cc123456>
2021-02-18 16:48:54 +0800

    realize by self

--- Java/Sliding_Window_Pattern_Practice_Questions/MaximumSubArrayOfSizeK.java
+++ Java/Sliding_Window_Pattern_Practice_Questions/MaximumSubArrayOfSizeK.java
@@ -9,16 +9,16 @@ class MaximumSubArrayOfSizeK
     public static void main (String args[])
     {
         //region TestCases
-        
+
         //first test case
         //int k = 2;
         //int[] arr = new int[] {2, 1, 5, 1, 3, 2};
-        
-        
+
+
         //2nd test case
-        int k = 2;
+        int k = 3;
         int[] arr = new int[] { 2, 3, 4, 1, 5 };
-        
+
         //endregion
 
         int result = MaximumSubArrayOfSizeK.findMaximumSubArray(arr, k);
@@ -28,19 +28,35 @@ class MaximumSubArrayOfSizeK
 
 
 
-    public static int findMaximumSubArray(int[] arr, int k)
-    {
+//    public static int findMaximumSubArray(int[] arr, int k)
+//    {
+//        int maxSum = 0;
+//        int windowSum = 0;
+//        int windowStarts = 0;
+//        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++)
+//        {
+//            windowSum += arr[windowEnd];
+//
+//            if (windowEnd >= k - 1) {
+//                maxSum = Math.max(maxSum, windowSum);
+//                windowSum -= arr[windowStarts];
+//                windowStarts++;
+//            }
+//        }
+//        return maxSum;
+//    }
+
+    public static int findMaximumSubArray(int[] arr, int k){
         int maxSum = 0;
         int windowSum = 0;
-        int windowStarts = 0;
-        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++)
-        {
-            windowSum += arr[windowEnd];
-
-            if (windowEnd >= k - 1) {
-                maxSum = Math.max(maxSum, windowSum);
-                windowSum -= arr[windowStarts];
-                windowStarts++;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            windowSum+=arr[windowEnd];
+            if (windowEnd>=k-1){
+                maxSum = Math.max(maxSum,windowSum);
+                //删除首位,保留重复sum
+                windowSum -=arr[windowStart];
+                windowStart++;
             }
         }
         return maxSum;
--- Java/Sliding_Window_Pattern_Practice_Questions/SmallestSubArrayOfSumK.java
+++ Java/Sliding_Window_Pattern_Practice_Questions/SmallestSubArrayOfSumK.java
@@ -9,19 +9,19 @@ class SmallestSubArrayOfSumK
     public static void main (String args[])
     {
         //region TestCases
-        
+
         //first test case
-        int sum = 8;
+        int sum = 10;
         int[] arr = new int[] {3, 4, 1, 1, 6};
-        
+
         //2nd test case
         //int sum = 7;
         //int[] arr = new int[] { 2, 1, 5, 2, 3, 2 };
-        
+
         //3rd test case
         //int sum = 7;
         //int[] arr = new int[] { 2, 1, 5, 2, 8 };
-        
+
         //endregion
 
         int result = SmallestSubArrayOfSumK.findSmallestSubArray(arr, sum);
@@ -31,25 +31,44 @@ class SmallestSubArrayOfSumK
 
 
 
-    public static int findSmallestSubArray(int[] arr, int k)
-    {
+//    public static int findSmallestSubArray(int[] arr, int k)
+//    {
+//        int minLength = Integer.MAX_VALUE;
+//        int windowStart = 0;
+//        int windowSum = 0;
+//        int windowEnd = 0;
+//        while(windowEnd < arr.length)
+//        {
+//            windowSum += arr[windowEnd];
+//
+//            while(windowSum >= k)
+//            {
+//                minLength = Math.min(minLength, windowEnd - windowStart + 1 );
+//                windowSum -= arr[windowStart];
+//                windowStart++;
+//
+//            }
+//            windowEnd++;
+//        }
+//        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+//    }
+
+    public static int findSmallestSubArray(int[] arr, int k){
         int minLength = Integer.MAX_VALUE;
-        int windowStart = 0;
+        int windowStart =0;
         int windowSum = 0;
-        int windowEnd = 0;
-        while(windowEnd < arr.length)
-        {
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
             windowSum += arr[windowEnd];
 
-            while(windowSum >= k)
-            {
-                minLength = Math.min(minLength, windowEnd - windowStart + 1 );
+            while (windowSum>=k){
+                minLength = Math.min(minLength,windowEnd-windowStart+1);
+                //缩短路径
                 windowSum -= arr[windowStart];
                 windowStart++;
-
             }
-            windowEnd++;
         }
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        return minLength ==Integer.MAX_VALUE ? 0 : minLength;
     }
 }
+
+
